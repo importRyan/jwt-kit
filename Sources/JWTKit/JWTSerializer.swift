d18584845c7a403db521bae9b9f5ae07427ec6f7
Ryan Ferrell <importRyan@gmail.com>
2022-06-20 16:12:54 -0700

    feat: Allow flexible header fields (temp workaround)

--- Sources/JWTKit/JWTSerializer.swift
+++ Sources/JWTKit/JWTSerializer.swift
@@ -40,3 +40,41 @@ struct JWTSerializer {
     }
 }
 
+struct FlexibleJWTSerializer {
+  func sign<Payload>(
+    _ payload: Payload,
+    using signer: JWTSigner,
+    typ: String = "JWT",
+    kid: JWKIdentifier,
+    customHeader: [String:String] = [:]
+  ) throws -> String
+  where Payload: JWTPayload
+  {
+    let jsonEncoder = JSONEncoder()
+    jsonEncoder.dateEncodingStrategy = .secondsSince1970
+
+    // encode header, copying header struct to mutate alg
+    var header = customHeader
+    header["alg"] = signer.algorithm.name
+    header["kid"] = kid.string
+    header["typ"] = typ
+
+    let headerData = try jsonEncoder.encode(header)
+    let encodedHeader = headerData.base64URLEncodedBytes()
+
+    // encode payload
+    let payloadData = try jsonEncoder.encode(payload)
+    let encodedPayload = payloadData.base64URLEncodedBytes()
+
+    // combine header and payload to create signature
+    let signatureData = try signer.algorithm.sign(encodedHeader + [.period] + encodedPayload)
+
+    // yield complete jwt
+    let bytes = encodedHeader
+    + [.period]
+    + encodedPayload
+    + [.period]
+    + signatureData.base64URLEncodedBytes()
+    return String(decoding: bytes, as: UTF8.self)
+  }
+}
